fff7ad1e088cc41f7e5a3cdb9b5ba082d75f76c0
rjulian <richard@rjulian.net>
2020-02-16 23:48:49 -0800

    Patch: add in passing full arn to environment variable

--- detect_security_group_open_ingress.tf
+++ detect_security_group_open_ingress.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+  data "aws_caller_identity" "current" {}
+
+  data "aws_region" "current" {}
+
+
 module "detect_security_group_open_ingress" {
   source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_lambda"
   rule_name        = "DetectSecurityGroupOpenIngress"
@@ -27,11 +32,12 @@ module "detect_security_group_open_ingress" {
 }
 PATTERN
 
+
   function_name            = "DetectSecurityGroupOpenIngress"
   source_code_dir          = "${path.module}/source"
   handler                  = "detect_security_group_open_ingress.lambda_handler"
   lambda_runtime           = "python3.7"
-  environment_variable_map = { SNS_TOPIC = "DetectSecurityGroupOpenIngress" }
+  environment_variable_map = { SNS_TOPIC = "arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:DetectSecurityGroupOpenIngress" }
   custom_lambda_policy     = <<EOF
 {
   "Version": "2012-10-17",
