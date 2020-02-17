c556af7d9d138f8ffd6ac4bd35df2a763b66c20d
rjulian <richard@rjulian.net>
2020-02-17 11:30:48 -0800

    Patch: fix sns topic arn

--- detect_security_group_open_ingress.tf
+++ detect_security_group_open_ingress.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-  data "aws_caller_identity" "current" {}
-
-  data "aws_region" "current" {}
-
-
 module "detect_security_group_open_ingress" {
   source           = "git@github.com:cloudmitigator/reflex.git//modules/cwe_lambda"
   rule_name        = "DetectSecurityGroupOpenIngress"
@@ -37,7 +32,7 @@ PATTERN
   source_code_dir          = "${path.module}/source"
   handler                  = "detect_security_group_open_ingress.lambda_handler"
   lambda_runtime           = "python3.7"
-  environment_variable_map = { SNS_TOPIC = "arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:DetectSecurityGroupOpenIngress" }
+  environment_variable_map = { SNS_TOPIC = module.detect_security_group_open_ingress.sns_topic_arn  }
   custom_lambda_policy     = <<EOF
 {
   "Version": "2012-10-17",
