3490b1c920b80a95de517078e79aa5cfeaa90884
Richard Julian <richard@rjulian.net>
2020-05-16 11:14:11 -0700

    Correct path to source code.

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -3,7 +3,7 @@ module "sqs_lambda" {
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
   function_name            = "Ec2SecurityGroupOpenIngress"
-  source_code_dir          = "${path.module}/source"
+  source_code_dir          = "${path.module}/../../source"
   handler                  = "ec2_security_group_open_ingress.lambda_handler"
   lambda_runtime           = "python3.7"
   environment_variable_map = { SNS_TOPIC = var.sns_topic_arn }
