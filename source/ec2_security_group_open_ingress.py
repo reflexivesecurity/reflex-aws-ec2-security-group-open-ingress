818c9c6a4d245203f93c832848bfe77022e0ebe8
rjulian <richard@rjulian.net>
2020-09-13 14:45:06 -0700

    Semi-automated multi account push

--- source/ec2_security_group_open_ingress.py
+++ source/ec2_security_group_open_ingress.py
@@ -4,7 +4,7 @@ import json
 
 import boto3
 
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class Ec2SecurityGroupOpenIngress(AWSRule):
@@ -52,5 +52,8 @@ class Ec2SecurityGroupOpenIngress(AWSRule):
 def lambda_handler(event, _):
     """ Handles the incoming event """
     print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = Ec2SecurityGroupOpenIngress(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
